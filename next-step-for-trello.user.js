f3afd10e6f470b2dfada1ba486fba9b9a926e651
Adrien Joly <adrien@whyd.com>
2016-11-17 12:41:24 +0100

    missing semicolons

--- next-step-for-trello.user.js
+++ next-step-for-trello.user.js
@@ -78,8 +78,8 @@ const Announcement = (announcementId) => {
   };
   const getCookie = (name) =>
     document.cookie.split('; ').reduce((r, v) => {
-      const parts = v.split('=')
-      return parts[0] === name ? decodeURIComponent(parts[1]) : r
+      const parts = v.split('=');
+      return parts[0] === name ? decodeURIComponent(parts[1]) : r;
     }, '');
   const deleteCookie = (name, path) => {
     setCookie(name, '', -1, path);
