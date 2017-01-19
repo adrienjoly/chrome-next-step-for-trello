fc943f74c2f01288dca069e0e3e6a163d4d9b211
Adrien Joly <adrien@whyd.com>
2017-01-19 13:41:32 +0100

    updated build.sh script

--- build.sh
+++ build.sh
@@ -1,5 +1,30 @@
-VERSION=$(jq .version manifest.json)
-FILEPATH=./NextStep-v${VERSION//\"}.zip
+# this script requires environment variables: APP_ID and ACCESS_TOKEN
+source .env
+
+VERSION=$(jq --raw-output .version manifest.json)
+echo "Packing v$VERSION ..."
+
+FILEPATH="./NextStep-v$VERSION.zip"
 rm $FILEPATH &>/dev/null
 zip $FILEPATH * --no-dir-entries --exclude *.sh *.zip
-echo "=> Built package for Chrome Web Store, to:" $FILEPATH
+echo "=> Built package for Chrome Web Store, to: $FILEPATH"
+
+echo "Uploading archive to Chrome Web Store ..."
+curl \
+  -H "Authorization: Bearer $ACCESS_TOKEN"  \
+  -H "x-goog-api-version: 2" \
+  -X PUT \
+  -T $FILEPATH \
+  -v \
+  https://www.googleapis.com/upload/chromewebstore/v1.1/items/$APP_ID
+
+echo "Publishing update ..."
+curl \
+  -H "Authorization: Bearer $ACCESS_TOKEN"  \
+  -H "x-goog-api-version: 2" \
+  -H "Content-Length: 0" \
+  -X POST \
+  -v \
+  https://www.googleapis.com/chromewebstore/v1.1/items/$APP_ID/publish
+
+echo "=> done. :-)"
