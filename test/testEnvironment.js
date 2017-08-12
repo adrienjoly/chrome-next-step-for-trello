d4a95d66d730afe918914d33dbf78a7627e32419
Adrien Joly <adrien@whyd.com>
2017-08-12 14:28:11 +0200

    initial auto-fix using « standard » linter

--- test/testEnvironment.js
+++ test/testEnvironment.js
@@ -1,15 +1,15 @@
-var vm = require('vm');
-var fs = require('fs');
-var chrome = require('sinon-chrome');
+var vm = require('vm')
+var fs = require('fs')
+var chrome = require('sinon-chrome')
 
 // Stop the DEV_MODE const from causing an error
-chrome.runtime.getManifest.returns([]);
+chrome.runtime.getManifest.returns([])
 
 // Fake up a browser to stop calls that access the DOM from causing errors
-var MockBrowser = require('mock-browser').mocks.MockBrowser;
-var mock = new MockBrowser();
-var document = mock.getDocument();
-var window = mock.getWindow();
+var MockBrowser = require('mock-browser').mocks.MockBrowser
+var mock = new MockBrowser()
+var document = mock.getDocument()
+var window = mock.getWindow()
 var setInterval = function () {}
 
 // This is the context our tests will run in
@@ -18,12 +18,12 @@ var sandbox = {
   document,
   setInterval,
   returnValue: null
-};
-var context = vm.createContext(sandbox);
+}
+var context = vm.createContext(sandbox)
 
 // Load the next-step-for-trello code
 var code = fs.readFileSync('next-step-for-trello.user.js')
-const script = new vm.Script(code);
+const script = new vm.Script(code)
 script.runInContext(context)
 
 module.exports = {
