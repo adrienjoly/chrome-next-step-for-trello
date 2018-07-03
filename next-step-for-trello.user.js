dbcb22f6140202b36860bb1d0d35ac1b34eec8bc
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2018-07-03 17:27:41 +0200

    v1.9.0: replace "donate" by sponsor message

--- next-step-for-trello.user.js
+++ next-step-for-trello.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Next Step for Trello
-// @version 1.8.20
+// @version 1.9.0
 // @homepage https://adrienjoly.com/chrome-next-step-for-trello
 // @description Check tasks directly from your Trello boards.
 // @match https://trello.com/*
