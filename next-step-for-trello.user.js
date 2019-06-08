7827993b5cac24073fc12e188aeb033836bdc449
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-06-08 12:12:10 +0200

    chore: bump to v1.12.0

--- next-step-for-trello.user.js
+++ next-step-for-trello.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Next Step for Trello
-// @version 1.11.0
+// @version 1.12.0
 // @homepage https://adrienjoly.com/chrome-next-step-for-trello
 // @description Check tasks directly from your Trello boards.
 // @match https://trello.com/*
