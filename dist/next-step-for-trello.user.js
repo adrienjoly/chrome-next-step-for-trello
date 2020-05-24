eaffe4442746d33864616b8a82943a535be3df55
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-05-24 12:42:12 +0200

    fix(code): remove old references to head analytics

--- dist/next-step-for-trello.user.js
+++ dist/next-step-for-trello.user.js
@@ -400,7 +400,6 @@ function showToolbarSelector (btn) {
   const rect = btn.getBoundingClientRect()
   node.style = `top: ${rect.top + rect.height + window.scrollY}px; left: ${rect.left + window.scrollX}px;`
   node.classList.add('is-shown')
-  // heap.track('Click on toolbar button', {});
   analytics.trackEvent('Toolbar Button', 'show')
   return node
 }
@@ -636,11 +635,6 @@ const loadAnnouncement = () => fetch(getAssetURL('announcement.json'))
   .then((json) => MENU_ITEMS.push(Object.assign(json, {
     onClick: (evt) => announcement.setAsSeen()
   })))
-
-const injectAnalytics = () => injectJs(`
-  window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
-  heap.load("3050518868");
-`)
 */
 
 const getToken = () => injectJs(
